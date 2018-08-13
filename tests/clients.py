fc0636f9bb2340a3703d1805f4ab1357a041ca33
Sergei Maertens <sergei@maykinmedia.nl>
2018-08-13 17:36:13 +0200

    Fix client initialization

--- tests/clients.py
+++ tests/clients.py
@@ -10,9 +10,9 @@ BASE_DIR = os.path.abspath(os.path.join(
 
 CONFIG_FILE = os.path.join(BASE_DIR, 'config.yml')
 
-Client.load_config(CONFIG_FILE)
+Client.load_config(BASE_DIR, CONFIG_FILE)
 
-ztc_client = Client('ztc')
 zrc_client = Client('zrc')
-orc_client = Client('orc')
 drc_client = Client('drc')
+ztc_client = Client('ztc')
+orc_client = Client('orc')
