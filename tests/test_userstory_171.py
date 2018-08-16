04b30c6291067b50115f8551a00fb785e2e6a2b9
Sergei Maertens <sergei@maykinmedia.nl>
2018-08-16 15:38:26 +0200

    Integration test voor toevoegen document aan zaak
    Fixes vng-Realisatie/gemma-zaken#171

---
+++ tests/test_userstory_171.py
@@ -0,0 +1,41 @@
+"""
+Test dat het mogelijk is om een document toe te voegen voor een zaak.
+
+Ref: https://github.com/VNG-Realisatie/gemma-zaken/issues/171
+"""
+import base64
+
+from .constants import CATALOGUS_UUID, ZAAKTYPE_UUID
+
+
+def test_upload_document(png_file, zrc_client, ztc_client, drc_client):
+    zaaktype = ztc_client.retrieve('zaaktype', catalogus_uuid=CATALOGUS_UUID, uuid=ZAAKTYPE_UUID)
+    zaak = zrc_client.create('zaak', {
+        'zaaktype': zaaktype['url'],
+        'bronorganisatie': '517439943',
+        'startdatum': '2018-06-18',
+    })
+
+    # upload document in DRC
+    byte_content = png_file.getvalue()
+    base64_bytes = base64.b64encode(byte_content)
+
+    document = drc_client.create('enkelvoudiginformatieobject', {
+        'bronorganisatie': '517439943',
+        'creatiedatum': zaak['registratiedatum'],
+        'titel': 'afbeelding.png',
+        'auteur': 'Jos den Homeros',
+        'formaat': 'image/png',
+        'taal': 'dut',
+        'inhoud': base64_bytes.decode('utf-8')
+    })
+
+    assert 'identificatie' in document
+
+    # relateer document aan zaak
+    zio = drc_client.create('zaakinformatieobject', {
+        'zaak': zaak['url'],
+        'informatieobject': document['url'],
+    })
+
+    assert 'url' in zio
